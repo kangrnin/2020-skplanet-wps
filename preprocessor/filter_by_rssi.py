30dfda54739d6a0c3eb32a1a6906f56ebd27d2a1
kangrnin <kangrnin@gmail.com>
2020-10-26 02:56:24 +0900

    Make ML data only for files under ml_data/signal_data

--- preprocessor/filter_by_rssi.py
+++ preprocessor/filter_by_rssi.py
@@ -7,17 +7,17 @@ def filter_by_signal():
     filtered_path = (script_path / '../signal_data/filtered').resolve()
     filtered_path.mkdir(parents=True, exist_ok=True)
 
-    for data_path in raw_path.glob('**/*'):
-        print(data_path)
-        with open(data_path, 'r') as fr:
+    for file_path in raw_path.glob('**/*'):
+        print(file_path)
+        with open(file_path, 'r') as fr:
             filtered = []
             for line in [l for l in fr.read().split('\n') if len(l)>0]:
                 # received signal strength > 40%
                 if int(line.split(',')[1][:-1]) >= 40:
                     filtered.append(line)
 
-            with open(filtered_path / (data_path.stem+'_filtered.csv'), 'w+') as fw:
-                print(filtered_path / (data_path.stem+'_filtered.csv'))
+            with open(filtered_path / (file_path.stem+'_filtered.csv'), 'w+') as fw:
+                print(filtered_path / (file_path.stem+'_filtered.csv'))
                 fw.write('\n'.join(filtered))
                 
 if __name__ == '__main__':
