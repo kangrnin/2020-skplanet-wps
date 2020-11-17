ec6f90c79032604ffd4228349283636b6eb336cf
jjeongah <j_eah@naver.com>
2020-11-17 17:00:30 +0900

    number of wifi

--- collector/skplanet_start.py
+++ collector/skplanet_start.py
@@ -10,6 +10,7 @@ from pathlib import Path
 import pandas as pd
 from os import listdir
 from os.path import isdir, join, splitext
+from wifi_scan import get_wifis
 
 class indoor(QMainWindow,Ui_MainWindow): 
     def __init__(self):
@@ -35,28 +36,53 @@ class indoor(QMainWindow,Ui_MainWindow):
         #open(self.building_comboBox, RP_newButton).close()
         self.RP_comboBox.addItem(splitext(RP_newButton)[0])
         
+    def scanning(self):
+        label = QLabel("스캔중입니다. 조금만 기다려주세요", self.dialog)
+        label.setGeometry(120, 70, 500, 24)
+        label.move(100, 100)
+        label.setObjectName("label")
+        self.dialog.setWindowTitle('scanning')
+        self.dialog.setWindowModality(Qt.ApplicationModal)
+        self.dialog.resize(600, 400)
+        self.dialog.show()
 
     def scan(self):
         collect(self.building_comboBox.currentText(),self.RP_comboBox.currentText() ,1)
-
-        label = QLabel("스캔이 성공적으로 완료되었습니다", self.dialog)
-        label.setGeometry(120, 70, 250, 24)
+        #
+        a=0
+        b=0
+        wifis = get_wifis()
+        wifis = []
+        for i in range(1):
+          wifis += get_wifis()
+          for wifi in wifis:
+              b += 1
+              if int(wifi['signal']) > 40:
+                  a += 1       
+        #
+        label = QLabel("스캔이 성공적으로 완료됐습니다", self.dialog)
+        label.setGeometry(120, 70, 500, 24)
         label.move(100, 100)
         label.setObjectName("label")
 
-        #btnDialog = QPushButton("위치별 수집내역 확인", self.dialog)
-        #btnDialog.setGeometry(1700, 1000, 250, 50)
-        #btnDialog.move(130, 150)
-        #btnDialog.clicked.connect(self.scan_check)
+        label3 = QLabel(f"모든 wifi의 갯수는 {b} 입니다", self.dialog)
+        label3.setGeometry(120, 70, 700, 24)
+        label3.move(40, 200)
+        label3.setObjectName("label")
+
+        label2 = QLabel(f"threshold보다 높은 wifi의 갯수는 {a} 입니다", self.dialog)
+        label2.setGeometry(120, 70, 700, 24)
+        label2.move(40, 300)
+        label2.setObjectName("label")
 
         btnDialog2 = QPushButton("서버로 전송", self.dialog)
         btnDialog2.setGeometry(50, 50, 250, 50)
-        btnDialog2.move(50, 250)
+        btnDialog2.move(50, 400)
         btnDialog2.clicked.connect(self.dialog_close)
 
         btnDialog2 = QPushButton("데이터 전송 취소하기", self.dialog)
         btnDialog2.setGeometry(50, 50, 250, 50)
-        btnDialog2.move(300, 250)
+        btnDialog2.move(300, 400)
         btnDialog2.clicked.connect(self.dialog_close)
 
         self.dialog.setWindowTitle('scan complete')
