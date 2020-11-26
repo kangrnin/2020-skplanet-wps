149b2f8458fc6903f7666d1f1d5b0f1ed1d53b58
kangrnin <kangrnin@gmail.com>
2020-11-26 20:29:04 +0900

    Change Wi-Fi scan method

--- collector/collector.py
+++ collector/collector.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import pandas as pd
 from wifi_scan import get_wifis, enable_wifi
 
-def collect(position, rp, cnt):
+def collect(position, rp):
     #position = str(position)
 
     script_path = Path(__file__).parent
@@ -11,13 +11,38 @@ def collect(position, rp, cnt):
     data_path.mkdir(parents=True, exist_ok=True)
     (data_path/position).mkdir(parents=True, exist_ok=True)
 
-    wifis = []
-    for i in range(cnt):
-        wifis += get_wifis()
-        print('completed scan #'+str(i+1))
+    wifi_list = []
+    bssid_set = set()
+
+    overlap_cnt = 0
+    scan_cnt = 1
+    while True:
+        print('-----------------------------------------')
+
+        scan_wifis = get_wifis()
+        print('total bssid cnt : ', len(scan_wifis))
+
+        new_bssids = [ w['bssid'] for w in scan_wifis if w['bssid'] not in bssid_set ]
+        bssid_set.update(new_bssids)
+
+        print(len(new_bssids), 'new bssids : [ ', end='')
+        for bssid in new_bssids:
+            print('~'+bssid[-8:], end=', ')
+        print(' ]')
+
+        overlap_cnt = overlap_cnt+1 if len(new_bssids) == 0 else 0
+        
+        if overlap_cnt == 3:
+            break
+
+        if new_bssids:
+            wifi_list += scan_wifis
+
+        print('completed scan #', scan_cnt)
+        scan_cnt += 1
     
     # (position) 폴더 안의 (rp).csv 안에 저장
-    df = pd.DataFrame(wifis)
+    df = pd.DataFrame(wifi_list)
     df.to_csv(data_path/position/(rp+'.csv'), mode='a', index=False, header=False)
 
     # position, rp 정보 column을 추가해서 통합 데이터파일에도 저장
@@ -29,7 +54,6 @@ if __name__ == "__main__":
     while True:
         position = input('Enter Position : ')
         rp = input('Enter RP : ')
-        cnt = int(input('Enter # of scans : '))
-        collect(position, rp, cnt)
+        collect(position, rp)
         
         
