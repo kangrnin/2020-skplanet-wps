12f981b43289b5764dcfb2d9d93d1708267477b4
jjeongah <j_eah@naver.com>
2020-11-26 14:42:49 +0900

    Change GUI(no loading screen)

--- collector/skplanet.py
+++ collector/skplanet.py
@@ -18,40 +18,73 @@ class Ui_MainWindow(object):
 
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(800, 600)
+        MainWindow.resize(750, 600)
+        MainWindow.setStyleSheet("background-color: rgb(255, 255, 238);\n")
         self.dialog = QDialog()
-
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
+
+
         self.ScanButton = QtWidgets.QPushButton(self.centralwidget)
         self.ScanButton.setGeometry(QtCore.QRect(310, 470, 150, 46)) 
+        self.ScanButton.move(150, 400)
         self.ScanButton.setObjectName("ScanButton")
 
+        self.SendButton = QtWidgets.QPushButton(self.centralwidget)
+        self.SendButton.setGeometry(QtCore.QRect(400, 470, 150, 46)) 
+        self.SendButton.move(330, 400)
+        self.SendButton.setObjectName("SendButton")
+
+
+        self.LocationButton = QtWidgets.QPushButton(self.centralwidget)
+        self.LocationButton.setGeometry(QtCore.QRect(400, 470, 150, 46)) 
+        self.LocationButton.move(500, 400)
+        self.LocationButton.setObjectName("현 위치 확인")
+
+
         self.building_ID = QtWidgets.QLabel(self.centralwidget)
-        self.building_ID.setGeometry(QtCore.QRect(110, 70, 241, 24)) 
+        self.building_ID.setGeometry(QtCore.QRect(170, 70, 260, 40)) 
         self.building_ID.setObjectName("building_ID")
+        self.building_ID.move(144, 45)
         self.RP_ID = QtWidgets.QLabel(self.centralwidget)
-        self.RP_ID.setGeometry(QtCore.QRect(120, 240, 241, 24))
+        self.RP_ID.setGeometry(QtCore.QRect(170, 240, 260, 40))
         self.RP_ID.setObjectName("RP_ID")
-        #
+        self.RP_ID.move(144, 220)
+
+        
         self.building_comboBox = QtWidgets.QComboBox(self.centralwidget)
-        self.building_comboBox.setGeometry(QtCore.QRect(120, 100, 261, 41)) 
+        self.building_comboBox.setGeometry(QtCore.QRect(140, 90, 270, 45)) 
         self.building_comboBox.setObjectName("building_comboBox")
+        self.building_comboBox.addItem(" ")
+        self.building_comboBox.setStyleSheet("font: 10pt \"마루 부리OTF Beta \";\n"
+"background-color: rgb(255, 255, 255);\n"
+"border-color: rgb(239, 240, 255);\n"
+"color: rgb(3, 0, 94);")
+
+
         self.RP_comboBox = QtWidgets.QComboBox(self.centralwidget)
-        self.RP_comboBox.setGeometry(QtCore.QRect(120, 280, 261, 41))
+        self.RP_comboBox.setGeometry(QtCore.QRect(140, 270, 270, 45))
         self.RP_comboBox.setObjectName("RP_comboBox")
-        #
+        self.RP_comboBox.setStyleSheet("font: 10pt \"마루 부리OTF Beta\";\n"
+"background-color: rgb(255, 255, 255);\n"
+"border-color: rgb(239, 240, 255);\n"
+"color: rgb(3, 0, 94);"
+)
+
         self.line = QtWidgets.QFrame(self.centralwidget)
         self.line.setGeometry(QtCore.QRect(120, 190, 118, 3))
         self.line.setFrameShape(QtWidgets.QFrame.HLine)
         self.line.setFrameShadow(QtWidgets.QFrame.Sunken)
         self.line.setObjectName("line")
+
         self.building_newbutton = QtWidgets.QPushButton(self.centralwidget)
-        self.building_newbutton.setGeometry(QtCore.QRect(440, 90, 150, 46))
+        self.building_newbutton.setGeometry(QtCore.QRect(500, 90, 150, 46))
         self.building_newbutton.setObjectName("building_newbutton")
+
         self.RP_newbutton = QtWidgets.QPushButton(self.centralwidget)
-        self.RP_newbutton.setGeometry(QtCore.QRect(450, 280, 150, 46))
+        self.RP_newbutton.setGeometry(QtCore.QRect(500, 270, 150, 46))
         self.RP_newbutton.setObjectName("RP_newbutton")
+
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 38))
@@ -64,11 +97,12 @@ class Ui_MainWindow(object):
         self.retranslateUi(MainWindow)
         self.building_newbutton.clicked.connect(MainWindow.new_building)
         self.ScanButton.clicked.connect(MainWindow.scan)
+        self.SendButton.clicked.connect(MainWindow.send)
+        self.LocationButton.clicked.connect(MainWindow.location)
         self.RP_newbutton.clicked.connect(MainWindow.new_rp)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
         print(self.data_path)
-        #
         self.building_comboBox.clear()
         for building in listdir(self.data_path):
             if isdir(join(self.data_path, building)):
@@ -86,8 +120,62 @@ class Ui_MainWindow(object):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.ScanButton.setText(_translate("MainWindow", "scan"))
+        self.ScanButton.setStyleSheet("font: 9pt \"마루 부리OTF Beta\";\n"
+"background-color: rgb(239, 240, 255);\n"
+"border-style: outset;\n"
+"border-width: 3px;\n"
+"border-radius: 15px;\n"
+"border-color: rgb(161,161,255);\n"
+"padding: 1px;\n"
+)
+        self.SendButton.setText(_translate("MainWindow", "서버로 전송"))
+        self.SendButton.setStyleSheet("font: 9pt \"마루 부리OTF Beta\";\n"
+"background-color: rgb(239, 240, 255);\n"
+"border-style: outset;\n"
+"border-width: 3px;\n"
+"border-radius: 15px;\n"
+"border-color: rgb(161,161,255);\n"
+"padding: 1px;\n"
+)
+        self.LocationButton.setText(_translate("MainWindow", "현 위치 확인"))
+        self.LocationButton.setStyleSheet("font: 9pt \"마루 부리OTF Beta\";\n"
+"background-color: rgb(239, 240, 255);"
+"border-style: outset;\n"
+"border-width: 3px;\n"
+"border-radius: 15px;\n"
+"border-color: rgb(161,161,255);\n"
+"padding: 1px;\n")
+
         self.building_ID.setText(_translate("MainWindow", "건물을 입력하시오"))
+        self.building_ID.setStyleSheet("font: 9pt \"마루 부리OTF Beta\";\n"
+"border-style: outset;\n"
+"border-radius: 15px;\n"
+"border-color: black;\n"
+"padding: 1px;\n")
+
         self.RP_ID.setText(_translate("MainWindow", "구역을 입력하시오"))
+        self.RP_ID.setStyleSheet("font: 9pt \"마루 부리OTF Beta\";\n"
+"border-style: outset;\n"
+"border-radius: 15px;\n"
+"border-color: black;\n"
+"padding: 1px;\n")
+
         self.building_newbutton.setText(_translate("MainWindow", "new"))
         self.RP_newbutton.setText(_translate("MainWindow", "new"))
+
+        self.building_newbutton.setStyleSheet("font: 10pt \"마루 부리OTF Beta\";\n"
+"background-color: rgb(239, 240, 255);"
+"border-style: outset;\n"
+"border-width: 3px;\n"
+"border-radius: 15px;\n"
+"border-color: rgb(161,161,255);\n"
+"padding: 1px;\n"
+)
+        self.RP_newbutton.setStyleSheet("font: 10pt \"마루 부리OTF Beta\";\n"
+"background-color: rgb(239, 240, 255);"
+"border-style: outset;\n"
+"border-width: 3px;\n"
+"border-radius: 15px;\n"
+"border-color: rgb(161,161,255);\n"
+"padding: 1px;\n")
 
--- collector/skplanet_start.py
+++ collector/skplanet_start.py
@@ -1,16 +1,20 @@
-import sys
+import sys, threading, time, random
 import pyautogui
 from skplanet import Ui_MainWindow
 from PyQt5 import *
+from PyQt5 import QtCore
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtWidgets import QMessageBox
+
 from collector import collect
 from pathlib import Path
 import pandas as pd
 from os import listdir
 from os.path import isdir, join, splitext
 from wifi_scan import get_wifis
+
+from PyQt5 import QtTest
 
 class indoor(QMainWindow,Ui_MainWindow): 
     def __init__(self):
@@ -20,8 +24,6 @@ class indoor(QMainWindow,Ui_MainWindow):
         
     def new_building(self):
         building_newButton= pyautogui.prompt(title='건물 추가',text='추가하려는 새로운 건물 이름을 입력하시오')
-        #print(building_newButton)
-        #self.building_comboBox.addItem(building_newButton) #need new file
         position = building_newButton
         script_path = Path(__file__).parent
         data_path = script_path / '../signal_data'
@@ -31,84 +33,137 @@ class indoor(QMainWindow,Ui_MainWindow):
 
     def new_rp(self):
         RP_newButton= pyautogui.prompt(title='구역 추가',text='추가하려는 새로운 구역 이름을 입력하시오')
-        #print(RP_newButton)
-        print(self.building_comboBox)
-        #open(self.building_comboBox, RP_newButton).close()
         self.RP_comboBox.addItem(splitext(RP_newButton)[0])
-        
+    
+    def sleep(self,n):
+       QtTest.QTest.qWait(n*1000)
+
     def scanning(self):
-        label = QLabel("스캔중입니다. 조금만 기다려주세요", self.dialog)
-        label.setGeometry(120, 70, 500, 24)
-        label.move(100, 100)
-        label.setObjectName("label")
-        self.dialog.setWindowTitle('scanning')
-        self.dialog.setWindowModality(Qt.ApplicationModal)
-        self.dialog.resize(600, 400)
-        self.dialog.show()
+        self.dialog.setStyleSheet("background-color: rgb(255, 255, 238);\n")
+        i = 1
+        c = 50
+        A = 5
+        B = A-2
+        C = A-1
+        while i < A:
+            label0 = QLabel(f"{i}회 스캔중입니다.", self.dialog)
+            label0.setGeometry(120, 70, 200, 30)
+            label0.move(100, c)
+            label0.setStyleSheet("font: 9pt \"마루 부리OTF Beta\";\n"
+"background-color: rgb(255, 251, 194);"
+"border-style: outset;\n"
+"border-radius: 15px;\n"
+"padding: 1px;\n")
+            self.dialog.resize(900, 900)
+            self.dialog.show()
+            self.sleep(0.1)
+            self.dialog.setWindowTitle('scanning')
+            self.dialog.setWindowModality(Qt.ApplicationModal)
+            if i < B :
+                self.dialog.close()
+            i += 1
+            c += 50
+            if i == C:
+                self.scanning()
 
     def scan(self):
+        self.dialog.setStyleSheet("background-color: rgb(255, 255, 238);\n")
         collect(self.building_comboBox.currentText(),self.RP_comboBox.currentText() ,1)
-        #
+
         a=0
         b=0
         wifis = get_wifis()
         wifis = []
         for i in range(1):
-          wifis += get_wifis()
-          for wifi in wifis:
-              b += 1
-              if int(wifi['signal']) > 40:
-                  a += 1       
-        #
+            wifis += get_wifis()
+            for wifi in wifis:
+                b += 1
+                if int(wifi['signal']) > 40:
+                    a += 1      
+        self.dialog.close()
         label = QLabel("스캔이 성공적으로 완료됐습니다", self.dialog)
-        label.setGeometry(120, 70, 500, 24)
-        label.move(100, 100)
+        label.setGeometry(120, 70, 400, 30)
+        label.move(100, 300)
+        label.setStyleSheet("font: 9pt \"마루 부리OTF Beta\";\n"
+"background-color: rgb(239, 240, 255);"
+"background-color: rgb(255, 251, 194);"
+"border-style: outset;\n"
+"border-radius: 15px;\n"
+"padding: 1px;\n")
         label.setObjectName("label")
 
         label3 = QLabel(f"모든 wifi의 갯수는 {b} 입니다", self.dialog)
-        label3.setGeometry(120, 70, 700, 24)
-        label3.move(40, 200)
+        label3.setGeometry(120, 70, 400, 30)
+        label3.move(100, 350)
         label3.setObjectName("label")
+        label3.setStyleSheet("font: 9pt \"마루 부리OTF Beta\";\n"
+"background-color: rgb(239, 240, 255);"
+"background-color: rgb(255, 251, 194);"
+"border-style: outset;\n"
+"border-radius: 15px;\n"
+"padding: 1px;\n")
 
         label2 = QLabel(f"threshold보다 높은 wifi의 갯수는 {a} 입니다", self.dialog)
-        label2.setGeometry(120, 70, 700, 24)
-        label2.move(40, 300)
+        label2.setGeometry(120, 70, 470, 30)
+        label2.move(100, 400)
         label2.setObjectName("label")
+        label2.setStyleSheet("font: 9pt \"마루 부리OTF Beta\";\n"
+"background-color: rgb(239, 240, 255);"
+"background-color: rgb(255, 251, 194);"
+"border-style: outset;\n"
+"border-radius: 15px;\n"
+"padding: 1px;\n")
 
         btnDialog2 = QPushButton("서버로 전송", self.dialog)
-        btnDialog2.setGeometry(50, 50, 250, 50)
-        btnDialog2.move(50, 400)
-        btnDialog2.clicked.connect(self.dialog_close)
+        btnDialog2.setGeometry(50, 50, 200, 50)
+        btnDialog2.move(100, 500)
+        btnDialog2.setStyleSheet("font: 9pt \"마루 부리OTF Beta\";\n"
+"background-color: rgb(198, 199, 255);"
+"border-style: outset;\n"
+"border-width: 2px;\n"
+"border-radius: 15px;\n"
+"border-color: black;\n"
+"padding: 1px;\n")
+        btnDialog2.clicked.connect(self.scan_check)
 
         btnDialog2 = QPushButton("데이터 전송 취소하기", self.dialog)
         btnDialog2.setGeometry(50, 50, 250, 50)
-        btnDialog2.move(300, 400)
-        btnDialog2.clicked.connect(self.dialog_close)
+        btnDialog2.move(320, 500)
+        btnDialog2.setStyleSheet("font: 9pt \"마루 부리OTF Beta\";\n"
+"background-color: rgb(198, 199, 255);"
+"border-style: outset;\n"
+"border-width: 2px;\n"
+"border-radius: 15px;\n"
+"border-color: black;\n"
+"padding: 1px;\n")
+        btnDialog2.clicked.connect(self.remove_data)
 
-        self.dialog.setWindowTitle('scan complete')
-        self.dialog.setWindowModality(Qt.ApplicationModal)
-        self.dialog.resize(600, 400)
         self.dialog.show()
+        self.dialog.resize(800, 700)
+        self.dialog.setWindowTitle('scan complete')
+        self.dialog.setStyleSheet("background-color: rgb(255, 255, 238);\n")
+        self.dialog.setWindowModality(Qt.ApplicationModal)
 
     def dialog_close(self):
         self.dialog.close()
 
+    def send(self):
+        print("send")
+
+    def location(self):
+        print("location")
+
+    def remove_data(self): #remove .csv file
+        #self.dialog.close()
+        #directory = f"./{building_comboBox.currentText()}/{RP_comboBox.currentText()}"
+        #os.remove(directory)
+         btn_2= pyautogui.alert("데이터 전송이 취소되었습니다.")
+         self.dialog.close()
+
+    # execute detector 
     def scan_check(self):
-        #label2 = QLabel("찾으시려는 새로운 건물 이름을 입력하시오", self.dialog)
-        #label2.setGeometry(120, 70, 250, 24)
-        #label2.setObjectName("label")
-
-        #self.dialog.setWindowTitle('scan complete')
-        #self.dialog.setWindowModality(Qt.ApplicationModal)
-        #self.dialog.resize(600, 400)
-        #self.dialog.show()
-        #buildingfind_newButton= pyautogui.prompt(title='건물 및 구역 확인', text='찾으시려는 새로운 건물과 구역 이름을 입력하시오\n ex: 반도체관 6층 / opensource2(400621)')
-        #if buildingfind_newButton == '반도체관 6층 / opensource2(400621)':
-           btn_1= pyautogui.alert("스캔이 성공적으로 완료되었습니다")
-
-       # if buildingfind_newButton == '반도체관 6층 / opensource1(400620)':
-       #     btn_1= pyautogui.alert("스캔이 성공적으로 완료되었습니다")
-
+           btn_1= pyautogui.alert("서버에 성공적으로 전송되었습니다.")
+           self.dialog.close()
 
 app =QApplication([])
 main_dialog = indoor() #해당부분 위 class name과 동일하게 작성
